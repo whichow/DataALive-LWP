f31fd012acfc617ffbc7435dd9d5ce1b6acba0fe
whichow <whichow@hotmail.com>
2022-02-06 22:01:40 +0800

    first commit

---
+++ Assets/Live2DModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Live2DModel : MonoBehaviour
+{
+    public string[] motionNames;
+    public string idleMotion;
+    public string idleExpression;
+    private LAppModel model;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LAppModelProxy proxy = GetComponent<LAppModelProxy>();
+        model = proxy.GetModel();
+    }
+
+    void OnMouseDown()
+    {
+        while(true)
+        {
+            int index = Random.Range(0, motionNames.Length);
+            string motionName = motionNames[index];
+            if(motionName != idleMotion)
+            {
+                model.StartMotion(motionName, 0, 2);
+                // model.SetRandomExpression();
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     if(model.IsFinished())
+    //     {
+    //         model.StartMotion(idleMotion, 0, 1);
+    //         // model.SetExpression(idleExpression);
+    //     }
+    // }
+}
---
+++ Assets/Live2DModelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Live2DModelManager : MonoBehaviour
+{
+    public GameObject[] models;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var model in models)
+        {
+            model.SetActive(false);
+        }
+        StartCoroutine(ShowModels());
+    }
+
+    IEnumerator ShowModels()
+    {
+        int i = 0;
+        while(true)
+        {
+            if(i == models.Length)
+            {
+                break;
+            }
+            if(i > 0)
+            {
+                models[i - 1].SetActive(false);
+            }
+            models[i].SetActive(true);
+            i++;
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
