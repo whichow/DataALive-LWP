3bde2246fa2e19228de3daa2d9549a7af027d228
whichow <whichow@hotmail.com>
2022-02-07 22:38:51 +0800

    add set wallpaper and switch role button

---
+++ Assets/SetWallpaperButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetWallpaperButton : MonoBehaviour
+{
+    private AndroidJavaClass activityClass;
+    private AndroidJavaClass wrapperClass;
+    private AndroidJavaObject wrapperObject;
+
+    private string activity = "ulw.ulw.ulw.UnityPlayerActivity";
+    private string wrapper = "ulw.ulw.ulw.Wrapper";
+
+    void Start()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        activityClass = new AndroidJavaClass(activity);
+        wrapperClass = new AndroidJavaClass(wrapper);
+        wrapperObject = wrapperClass.CallStatic<AndroidJavaObject>("instance");
+#endif
+    }
+
+    public void SetAsWallpaper()
+    {
+        StartWallpaperService();
+    }
+
+    public void StartWallpaperService()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        activityClass.CallStatic("StartService");
+#endif
+    }
+}
---
+++ Assets/SwitchRoleButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchRoleButton : MonoBehaviour
+{
+    public GameObject[] roles;
+
+    private int index;
+
+    void Start()
+    {
+        for(int i = 0; i < roles.Length; i++)
+        {
+            if(roles[i].activeSelf == true)
+            {
+                index = i;
+            }
+        }
+    }
+
+    public void SwitchRandomRole()
+    {
+        roles[index].SetActive(false);
+        index = Random.Range(0, roles.Length);
+        roles[index].SetActive(true);
+    }
+
+    public void SwitchNextRole()
+    {
+
+    }
+
+    public void SwitchPreviousRole()
+    {
+
+    }
+}
