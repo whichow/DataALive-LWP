74b468e3292a997e30ceca1370031e08ba42d437
wuhao <wuhao@orbbec.com>
2022-09-05 20:41:40 +0800

    防止误触

--- Assets/Live2DModel.cs
+++ Assets/Live2DModel.cs
@@ -7,6 +7,9 @@ public class Live2DModel : MonoBehaviour
     public string[] motionNames;
     private LAppModel model;
 
+    private float mouseDownTime;
+    private Vector3 mousePos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,24 @@ public class Live2DModel : MonoBehaviour
 
     void OnMouseDown()
     {
+        Debug.Log("On mouse down");
+        mouseDownTime = Time.time;
+        mousePos = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("On mouse up");
+        if (Time.time - mouseDownTime > 0.5f)
+        {
+            return;
+        }
+        var deltaPos = Vector3.Distance(Input.mousePosition, mousePos);
+        if (deltaPos > 0.1f)
+        {
+            return;
+        }
+        Debug.Log("On mouse click");
         if(model.IsFinished())
         {
             int index = Random.Range(0, motionNames.Length);
