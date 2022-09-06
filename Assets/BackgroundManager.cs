5888c75836b8314b9d141c7f1fcbc50431aba22c
wuhao <wuhao@orbbec.com>
2022-09-06 16:27:31 +0800

    添加视频背景

---
+++ Assets/BackgroundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public GameObject[] backgrounds;
+
+    public bool random;
+
+    private int index;
+
+    void Start()
+    {
+        backgrounds[index].SetActive(true);
+        if(random)
+        {
+            SwitchBackground();
+        }
+    }
+
+    public void SwitchBackground()
+    {
+        backgrounds[index].SetActive(false);
+        if(random)
+        {
+            index = Random.Range(0, backgrounds.Length);
+        }
+        else
+        {
+            if(++index >= backgrounds.Length)
+                index = 0;
+        }
+        backgrounds[index].SetActive(true);
+    }
+}
