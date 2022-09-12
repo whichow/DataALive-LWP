8c0bfea314260f2dce7bcea1e69a390c9ed972c8
echxmmzq <93745171+echxmmzq@users.noreply.github.com>
2022-09-12 13:50:22 +0800

    修复不能静音问题

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public void EnableVolume(bool enable)
+    {
+        if(enable)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+        }
+    }
+}
